d78d5fe19b948c02ceae3b87cd3f9789a9ad6283
LBigtou <56402445+LBigtou@users.noreply.github.com>
2024-08-20 15:22:07 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'http://app.cgfw.uk/'
+window.settings = {
+  // 站点名称
+  title: 'GFW',
+  // 站点描述
+  description: '翻越 Great Firewall !!!',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'default', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '6.8.2',
+  // 登陆背景
+  background_url: '',
+  // 站点LOGO
+  logo: '',
+}
